21569162d6e73bc431218025d0622eb303166906
Andrew Minnich <aminnich3@gmail.com>
2021-10-10 13:59:57 -0400

    use an inner function for (hopefully) better compile times

--- kira/src/sound/static_sound.rs
+++ kira/src/sound/static_sound.rs
@@ -44,112 +44,116 @@ impl StaticSound {
 		path: impl AsRef<Path>,
 		settings: StaticSoundSettings,
 	) -> Result<Self, FromFileError> {
-		use std::{fs::File, io::ErrorKind};
+		fn inner(path: &Path, settings: StaticSoundSettings) -> Result<StaticSound, FromFileError> {
+			use std::{fs::File, io::ErrorKind};
 
-		use symphonia::core::{
-			audio::{AudioBuffer, AudioBufferRef, Signal},
-			conv::IntoSample,
-			errors::Error,
-			io::MediaSourceStream,
-			probe::Hint,
-			sample::Sample,
-		};
+			use symphonia::core::{
+				audio::{AudioBuffer, AudioBufferRef, Signal},
+				conv::IntoSample,
+				errors::Error,
+				io::MediaSourceStream,
+				probe::Hint,
+				sample::Sample,
+			};
 
-		fn read_frames_from_buffer<T: Sample + IntoSample<f32>>(
-			frames: &mut Vec<Frame>,
-			buffer: &AudioBuffer<T>,
-		) -> Result<(), FromFileError> {
-			let num_channels = buffer.spec().channels.count();
-			if !(num_channels == 1 || num_channels == 2) {
-				return Err(FromFileError::UnsupportedChannelConfiguration);
+			fn read_frames_from_buffer<T: Sample + IntoSample<f32>>(
+				frames: &mut Vec<Frame>,
+				buffer: &AudioBuffer<T>,
+			) -> Result<(), FromFileError> {
+				let num_channels = buffer.spec().channels.count();
+				if !(num_channels == 1 || num_channels == 2) {
+					return Err(FromFileError::UnsupportedChannelConfiguration);
+				}
+				for i in 0..buffer.frames() {
+					match num_channels {
+						1 => frames.push(Frame::from_mono(buffer.chan(0)[i].into_sample())),
+						2 => frames.push(Frame::new(
+							buffer.chan(0)[i].into_sample(),
+							buffer.chan(1)[i].into_sample(),
+						)),
+						_ => unreachable!(),
+					}
+				}
+				Ok(())
 			}
-			for i in 0..buffer.frames() {
-				match num_channels {
-					1 => frames.push(Frame::from_mono(buffer.chan(0)[i].into_sample())),
-					2 => frames.push(Frame::new(
-						buffer.chan(0)[i].into_sample(),
-						buffer.chan(1)[i].into_sample(),
-					)),
-					_ => unreachable!(),
+
+			fn read_frames_from_buffer_ref(
+				frames: &mut Vec<Frame>,
+				buffer_ref: &AudioBufferRef,
+			) -> Result<(), FromFileError> {
+				match buffer_ref {
+					AudioBufferRef::U8(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::U16(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::U24(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::U32(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::S8(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::S16(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::S24(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::S32(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::F32(buffer) => read_frames_from_buffer(frames, buffer),
+					AudioBufferRef::F64(buffer) => read_frames_from_buffer(frames, buffer),
 				}
 			}
-			Ok(())
-		}
 
-		fn read_frames_from_buffer_ref(
-			frames: &mut Vec<Frame>,
-			buffer_ref: &AudioBufferRef,
-		) -> Result<(), FromFileError> {
-			match buffer_ref {
-				AudioBufferRef::U8(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::U16(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::U24(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::U32(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::S8(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::S16(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::S24(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::S32(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::F32(buffer) => read_frames_from_buffer(frames, buffer),
-				AudioBufferRef::F64(buffer) => read_frames_from_buffer(frames, buffer),
-			}
-		}
-
-		let file = File::open(path)?;
-		let codec_registry = symphonia::default::get_codecs();
-		let probe = symphonia::default::get_probe();
-		let media_source_stream = MediaSourceStream::new(Box::new(file), Default::default());
-		let probe_result = probe.format(
-			&Hint::new(),
-			media_source_stream,
-			&Default::default(),
-			&Default::default(),
-		)?;
-		let mut format_reader = probe_result.format;
-		let track = format_reader
-			.default_track()
-			.ok_or(FromFileError::NoAudio)?;
-		let mut decoder = codec_registry.make(&track.codec_params, &Default::default())?;
-		let mut frames = vec![];
-		let mut sample_rate = None;
-		loop {
-			match format_reader.next_packet() {
-				Ok(packet) => {
-					let buffer_ref = decoder.decode(&packet)?;
-					// if we've decoded a previous packet, make sure the sample rate
-					// of this packet matches (StaticSounds don't currently support
-					// variable sample rates). otherwise, set the sample rate now
-					if let Some(sample_rate) = sample_rate {
-						if sample_rate != buffer_ref.spec().rate {
-							return Err(FromFileError::VariableSampleRate);
+			let file = File::open(path)?;
+			let codec_registry = symphonia::default::get_codecs();
+			let probe = symphonia::default::get_probe();
+			let media_source_stream = MediaSourceStream::new(Box::new(file), Default::default());
+			let probe_result = probe.format(
+				&Hint::new(),
+				media_source_stream,
+				&Default::default(),
+				&Default::default(),
+			)?;
+			let mut format_reader = probe_result.format;
+			let track = format_reader
+				.default_track()
+				.ok_or(FromFileError::NoAudio)?;
+			let mut decoder = codec_registry.make(&track.codec_params, &Default::default())?;
+			let mut frames = vec![];
+			let mut sample_rate = None;
+			loop {
+				match format_reader.next_packet() {
+					Ok(packet) => {
+						let buffer_ref = decoder.decode(&packet)?;
+						// if we've decoded a previous packet, make sure the sample rate
+						// of this packet matches (StaticSounds don't currently support
+						// variable sample rates). otherwise, set the sample rate now
+						if let Some(sample_rate) = sample_rate {
+							if sample_rate != buffer_ref.spec().rate {
+								return Err(FromFileError::VariableSampleRate);
+							}
+						} else {
+							sample_rate = Some(buffer_ref.spec().rate);
 						}
-					} else {
-						sample_rate = Some(buffer_ref.spec().rate);
+						read_frames_from_buffer_ref(&mut frames, &buffer_ref)?;
 					}
-					read_frames_from_buffer_ref(&mut frames, &buffer_ref)?;
-				}
-				Err(Error::IoError(err)) => {
-					// UnexpectedEof is how we know we've reached the end of the
-					// audio, so break out of the loop. any other error is a
-					// legitimate error
-					if err.kind() == ErrorKind::UnexpectedEof {
-						break;
-					} else {
-						return Err(Error::IoError(err).into());
+					Err(Error::IoError(err)) => {
+						// UnexpectedEof is how we know we've reached the end of the
+						// audio, so break out of the loop. any other error is a
+						// legitimate error
+						if err.kind() == ErrorKind::UnexpectedEof {
+							break;
+						} else {
+							return Err(Error::IoError(err).into());
+						}
 					}
+					Err(err) => return Err(err.into()),
 				}
-				Err(err) => return Err(err.into()),
 			}
+			// if we haven't determined a sample rate yet, there must have been no
+			// packets, so return the NoAudio error
+			let sample_rate = sample_rate.ok_or(FromFileError::NoAudio)?;
+			let duration = Duration::from_secs_f64(frames.len() as f64 / sample_rate as f64);
+			Ok(StaticSound {
+				sample_rate,
+				duration,
+				frames,
+				default_loop_behavior: settings.default_loop_behavior,
+			})
 		}
-		// if we haven't determined a sample rate yet, there must have been no
-		// packets, so return the NoAudio error
-		let sample_rate = sample_rate.ok_or(FromFileError::NoAudio)?;
-		let duration = Duration::from_secs_f64(frames.len() as f64 / sample_rate as f64);
-		Ok(Self {
-			sample_rate,
-			duration,
-			frames,
-			default_loop_behavior: settings.default_loop_behavior,
-		})
+
+		inner(path.as_ref(), settings)
 	}
 }
 
